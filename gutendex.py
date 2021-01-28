2a75b199733c868597fb39b77196e36d461bca14
Rohit Swami <rowhitswami1@gmail.com>
2021-01-29 01:20:58 +0530

    Chores: Remove one-to-one relationship

--- gutendex.py
+++ gutendex.py
@@ -1,4 +1,5 @@
-import os, math
+import os
+import math
 from sqlalchemy import create_engine, or_, and_, func
 from sqlalchemy import Column, String, Integer, Numeric, SmallInteger, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
@@ -24,6 +25,7 @@ base.metadata.create_all(db)
 # Create schema of the database's table
 # Mapping their relationship with each other
 
+
 class Book(base):
     __tablename__ = 'books_book'
 
@@ -32,9 +34,8 @@ class Book(base):
     gutenberg_id = Column(Integer, unique=True, nullable=False)
     media_type = Column(String, nullable=False)
     title = Column(String)
-    author = relationship('Book_Authors', backref='books_book', uselist=False)
-    language = relationship(
-        'Book_Languages', backref='books_book',  uselist=False)
+    author = relationship('Book_Authors', backref='books_book')
+    language = relationship('Book_Languages', backref='books_book')
     bookshelves = relationship('Book_Bookshelves', backref='books_book')
     subjects = relationship('Book_Subjects', backref='books_book')
     formats = relationship('Format', backref='books_book')
@@ -51,14 +52,14 @@ class Author(base):
 
 class Bookshelf(base):
     __tablename__ = 'books_bookshelf'
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
 
 
 class Format(base):
     __tablename__ = 'books_format'
-    
+
     id = Column(Integer, primary_key=True)
     mime_type = Column(String, nullable=False)
     url = Column(String, nullable=False)
@@ -67,21 +68,21 @@ class Format(base):
 
 class Language(base):
     __tablename__ = 'books_language'
-    
+
     id = Column(Integer, primary_key=True)
     code = Column(String, unique=True, nullable=False)
 
 
 class Subject(base):
     __tablename__ = 'books_subject'
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
 
 class Book_Authors(base):
     __tablename__ = 'books_book_authors'
-    
+
     id = Column(Integer, primary_key=True)
     book_id = Column(Integer, ForeignKey('books_book.id'), nullable=False)
     author_id = Column(Integer, ForeignKey('books_author.id'), nullable=False)
@@ -91,7 +92,7 @@ class Book_Authors(base):
 
 class Book_Bookshelves(base):
     __tablename__ = 'books_book_bookshelves'
-    
+
     id = Column(Integer, primary_key=True)
     book_id = Column(Integer, ForeignKey('books_book.id'), nullable=False)
     bookshelf_id = Column(Integer, ForeignKey(
@@ -101,7 +102,7 @@ class Book_Bookshelves(base):
 
 class Book_Languages(base):
     __tablename__ = 'books_book_languages'
-    
+
     id = Column(Integer, primary_key=True)
     book_id = Column(Integer, ForeignKey('books_book.id'), nullable=False)
     language_id = Column(Integer, ForeignKey(
@@ -111,7 +112,7 @@ class Book_Languages(base):
 
 class Book_Subjects(base):
     __tablename__ = 'books_book_subjects'
-    
+
     id = Column(Integer, primary_key=True)
     book_id = Column(Integer, ForeignKey('books_book.id'), nullable=False)
     subject_id = Column(Integer, ForeignKey(
@@ -119,17 +120,19 @@ class Book_Subjects(base):
     subjects = relationship('Subject', backref='books_book_subjects')
 
 # Handling the query
+
+
 class Queries():
-    
+
     def __init__(self):
         self.session = Session()
         self.per_page = 25
-        
-    def search_books(self,book_ids, languages, mime_types, authors, titles, topics, page):
+
+    def search_books(self, book_ids, languages, mime_types, authors, titles, topics, page):
         """
         Function to return the matched query to retrieve books
         """
-        
+
         query = self.session.query(Book.id)
 
         books_to_return = []
@@ -137,7 +140,7 @@ class Queries():
 
         # Filters with multiple criteria
         if book_ids:
-            query = query.filter(Book.id.in_(book_ids))
+            query = query.filter(Book.gutenberg_id.in_(book_ids))
 
         if languages:
             query = query.join(Book_Languages).join(
@@ -160,13 +163,13 @@ class Queries():
             bookshelf_ids = query.join(Book_Bookshelves).join(Bookshelf).filter(
                 or_(func.lower(Bookshelf.name).contains(item.lower()) for item in topics)).all()
             books_to_return = list(set(subject_ids + bookshelf_ids))
-        
+
         if not books_to_return:
             books_to_return = list(set(query.all()))
 
         result = self.session.query(Book).filter(Book.id.in_(
             books_to_return)).order_by(Book.download_count.desc())
-        
+
         # Pagination
         try:
             paginated_results = pager(result, page, self.per_page)
@@ -178,13 +181,15 @@ class Queries():
         total_result = len(books_to_return)
         page = paginated_results.page
         total_page = paginated_results.pages
-        
+
         # Creating books object in required format
         output = []
         for book in paginated_results.items:
             one_book = {}
             bookshelves = []
             subjects = []
+            authors = []
+            languages = []
             download_links = {}
 
             for bookshelf in book.bookshelves:
@@ -194,20 +199,24 @@ class Queries():
             for link in book.formats:
                 download_links[link.mime_type] = link.url
 
-            one_book['title'] = book.title
-            one_book['genre'] = book.media_type
             if book.author:
-                one_book['author'] = book.author.author.name
+                for author in book.author:
+                    authors.append(author.author.name)
             else:
-                one_book['author'] = None
+                authors = None
 
             if book.language:
-                one_book['language'] = book.language.language.code
+                for language in book.language:
+                    languages.append(language.language.code)
             else:
-                one_book['language'] = None
+                languages = None
 
+            one_book['title'] = book.title
+            one_book['genre'] = book.media_type
             one_book['bookshelves'] = bookshelves
             one_book['subjects'] = subjects
+            one_book['authors'] = authors
+            one_book['languages'] = languages
 
             one_book['download_links'] = download_links
             output.append(one_book)
